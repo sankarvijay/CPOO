d33d06593a80c54b4ff2130219041881212a1c93
sankarvijay <vijay93700@gmail.com>
2017-10-13 18:07:50 +0200

    goupload
    Add Logging

--- TD-Banque/src/main/Main.java
+++ TD-Banque/src/main/Main.java
@@ -1,6 +1,13 @@
 package main;
 
 import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.Handler;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.io.IOException;
+import java.util.logging.SimpleFormatter ;
+
 
 import model.dec.CompteManip;
 import model.fact.CompteFactory;
@@ -11,21 +18,39 @@ public final class Main {
 
     public static void main (String[] args) {
 
-    	CompteFactory cf = new CompteFactory();
+        try{
+            Handler handler = new FileHandler("bank.log",true);
+            handler.setLevel(Level.FINEST);
+            LOG.addHandler(handler);
+
+        handler.setFormatter(new SimpleFormatter());
+        handler.setLevel(Level.FINE);
+        LOG.addHandler(handler);
+        LOG.setLevel(Level.FINE);
+        }catch(IOException ioe){
+            LOG.warning("Fichier introuvable !");
+        }
+
+        CompteFactory cf = new CompteFactory();
     	
     	CompteManip c1 = cf.createCompteCourant("John Doe");
     	CompteManip c2 = cf.createCompteCourant("Jack Bauer");
 
     	c1.deposit(1500.0d);
+        double amount = 1900.0d;
         try{
-            double retrait = c1.withdraw(1900.0d);
-            
+            double retrait = c1.withdraw(69500.0d);  
         }catch(InsufficientBalance ex){
-            LOG.error(ex);
+            LOG.severe(ex.getMessage());
+        }finally {
+           LOG.fine("Withdraw operation of Vijay" + amount);
+            LOG.log(Level.FINE, "VIIIIIIIIIIIIIIIIIJAY");
+            LOG.log(Level.FINER, "VIIIIIIIIIIIIIIIIIJAY");
+            LOG.log(Level.FINEST, "VIIIIIIIIIIIIIIIIIJAY");
+
         }
         double newSolde = c2.deposit(100000.0d);
-
-        System.out.println(c1);
-        System.out.println(c2);
+        LOG.info(c1.toString());
+        System.out.println(c2.toString());
     }
 }
--- TD-Banque/src/model/impl/CompteCourant.java
+++ TD-Banque/src/model/impl/CompteCourant.java
@@ -2,6 +2,8 @@ package model.impl;
 
 import java.util.UUID;
 import java.lang.StringBuilder;
+import java.util.logging.Logger;
+
 
 import model.dec.CompteManip;
 import model.exceptions.InsufficientBalance;
@@ -10,6 +12,8 @@ import model.exceptions.InsufficientBalance;
  * Modelsa check account.
  */
 public final class CompteCourant extends Compte implements CompteManip {
+
+    private static final Logger LOG = Logger.getLogger(CompteCourant.class.getCanonicalName());
     
     private int overdraft;
 
@@ -45,8 +49,13 @@ public final class CompteCourant extends Compte implements CompteManip {
     public double withdraw(double amount) throws InsufficientBalance{
         double previous = this.getBalance();
 
-        if(previous - amount < 0.0d) {
-            throw new InsufficientBalance("Overdraft. Current balance ="+previous);
+        try{
+            if(previous - amount < 0.0d) {
+                throw new InsufficientBalance("Overdraft. Current balance = "+previous);
+            }
+        }catch(InsufficientBalance ex){
+            LOG.severe("Overdraft of "+(previous-amount)+" on account with balance "+previous);
+            throw ex;
         }
         return this.getBalance();
     }
